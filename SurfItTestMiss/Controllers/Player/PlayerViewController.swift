790e2ecbffa3f983c0cb2f3b891caa4e21342261
Weeapa <77641616+Weeapa@users.noreply.github.com>
2023-09-06 14:28:19 +0300

    add PlayerView and PlayerViewController

---
+++ SurfItTestMiss/Controllers/Player/PlayerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerViewController.swift
+//  SurfItTestMiss
+//
+//  Created by Калякин Дима  on 06.09.2023.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+final class PlayerViewController: UIViewController{
+    
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var PreviousButton: UIButton!
+    @IBOutlet weak var progressSlider: UISlider!
+
+    
+    
+}
